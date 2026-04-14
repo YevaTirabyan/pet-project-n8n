19fb9a0dd6d67eb0ba084fe06d099359c6b0f97b
Yeva Tirabyan <68999736+YevaTirabyan@users.noreply.github.com>
2026-04-14 12:28:21 +0400

    Add a “Rainbow” button that, when clicked, changes the background color in sequence through the colors of the rainbow.

---
+++ button.js
@@ -0,0 +1,11 @@
+const rainbowButton = document.createElement('button');
+rainbowButton.innerText = 'Rainbow';
+document.body.appendChild(rainbowButton);
+
+const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3'];
+let index = 0;
+
+rainbowButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = colors[index];
+    index = (index + 1) % colors.length;
+});
